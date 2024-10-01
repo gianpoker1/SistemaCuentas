531a3ff9628ecbfa18ee303b8a4314ac565e434d
giancarlo Berninzon <giancarlo.berninzon.2@gmail.com>
2024-10-01 22:39:03 +0200

    funcionalidad agregar, editar y eliminar

--- src/main/java/gian/cuentas/controller/IndexController.java
+++ src/main/java/gian/cuentas/controller/IndexController.java
@@ -3,8 +3,11 @@ package gian.cuentas.controller;
 import gian.cuentas.model.Cuenta;
 import gian.cuentas.service.CuentaService;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,7 @@ public class IndexController {
     @Autowired
     private CuentaService cuentaService;
     private List<Cuenta> cuentas;
+    private Cuenta cuentaSeleccionada;
 
     private static final Logger logger= LoggerFactory.getLogger(IndexController.class);
 
@@ -32,6 +36,50 @@ public class IndexController {
     public void cargarData(){
         this.cuentas = cuentaService.listarCuentas();
         cuentas.forEach((cuenta) -> logger.info(cuenta.toString()));
+    }
+
+    public void agregarCuenta(){
+        this.cuentaSeleccionada = new Cuenta();
+
+    }
+
+    public void guardarCuenta(){
+        logger.info("Cuenta a guardar: " + this.cuentaSeleccionada);
+        //Guardar
+        if(this.cuentaSeleccionada.getIdCuenta() == null){
+            this.cuentaService.saveCuenta(this.cuentaSeleccionada);
+            //agregar la cuenta a la lista de cuentas (tabla)
+            this.cuentas.add(this.cuentaSeleccionada);
+            FacesContext.getCurrentInstance().addMessage(
+                    null, new FacesMessage("Cuenta agregada con exito"));
+        }//editar
+        else {
+            this.cuentaService.saveCuenta(cuentaSeleccionada);
+            FacesContext.getCurrentInstance().addMessage(
+                    null, new FacesMessage("Cuenta actualizada con exito"));
+
+        }
+        //cerramos la ventana
+        PrimeFaces.current().executeScript("PF('ventanaModalCuenta').hide()");
+        //actualizamos la tabla
+        PrimeFaces.current().ajax().update("forma-cuentas:mensajes",
+                "forma-cuentas:cuentas-tabla");
+        // volver el objeto a null
+        this.cuentaSeleccionada = null;
+    }
+
+    public void eliminarCuenta(){
+        logger.info("Cuenta a eliminar: " +this.cuentaSeleccionada);
+        this.cuentaService.deleteByIdCuenta(this.cuentaSeleccionada.getIdCuenta());
+        //eliminar el registro de la lista de cuentas
+        this.cuentas.remove(this.cuentaSeleccionada);
+        // volver el objeto a null
+        this.cuentaSeleccionada = null;
+        FacesContext.getCurrentInstance().addMessage(
+                null, new FacesMessage("Cuenta eliminada con exito"));
+        //actualizamos la tabla
+        PrimeFaces.current().ajax().update("forma-cuentas:mensajes",
+                "forma-cuentas:cuentas-tabla");
     }
 
 
